1fbd155abb9b8fd2daab8d6ac7e12b077da94780
victor.hugo <victorhpantunes@gmail.com>
2022-02-12 00:00:27 -0300

    Adding style to home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -51,51 +51,88 @@ class _HomePageState extends State<HomePage> {
               );
             }
 
-            return GridView.count(
-              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
-              crossAxisCount: 2,
-              crossAxisSpacing: _axisSpacing,
-              mainAxisSpacing: _axisSpacing,
-              children: [
-                for (final Dado dado in snapshot.data!.dados)
-                  Material(
-                    elevation: 10,
-                    color: CupertinoColors.tertiarySystemGroupedBackground,
-                    borderRadius: BorderRadius.circular(20),
-                    child: Container(
-                      clipBehavior: Clip.antiAlias,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          SizedBox(
-                            height: _size.height * 0.009,
+            /// Uses a [SliverGridDelegateWithFixedCrossAxisCount] as the
+            /// [gridDelegate], and a [SliverChildListDelegate] as the [delegate].
+
+            double axisSpacing = _size.height * 0.01;
+            List<Dado> dados = snapshot.data!.dados;
+            return SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 60),
+                child: CustomScrollView(slivers: [
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                        (BuildContext context, int index) {
+                      return const Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                        child: CupertinoTextField(
+                          placeholder: 'Buscar',
+                          prefix: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Icon(
+                              CupertinoIcons.search,
+                              color: Colors.grey,
+                            ),
                           ),
-                          Material(
-                            elevation: 11,
-                            borderRadius: BorderRadius.circular(15),
-                            child: Container(
+                        ),
+                      );
+                    }, childCount: 1),
+                  ),
+                  SliverGrid.count(
+                      mainAxisSpacing: axisSpacing,
+                      crossAxisSpacing: axisSpacing,
+                      crossAxisCount: 2,
+                      children: [
+                        for (final Dado dado in dados)
+                          Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Material(
+                              elevation: 10,
+                              color: CupertinoColors
+                                  .tertiarySystemGroupedBackground,
+                              borderRadius: BorderRadius.circular(20),
+                              child: Container(
                                 clipBehavior: Clip.antiAlias,
                                 decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(15)),
-                                height: _size.height * 0.13,
-                                child: Image.network(dado.urlFoto)),
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    SizedBox(
+                                      height: _size.height * 0.009,
+                                    ),
+                                    Material(
+                                      elevation: 11,
+                                      borderRadius: BorderRadius.circular(15),
+                                      child: Container(
+                                          clipBehavior: Clip.antiAlias,
+                                          decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(15)),
+                                          height: _size.height * 0.13,
+                                          child: Image.network(dado.urlFoto)),
+                                    ),
+                                    Expanded(
+                                        child: Center(child: Text(dado.nome))),
+                                    Expanded(
+                                      child: Container(
+                                          alignment: Alignment.center,
+                                          constraints:
+                                              const BoxConstraints.expand(),
+                                          color: ColorLib.green.color,
+                                          child: Text(dado.siglaPartido)),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
                           ),
-                          Expanded(child: Center(child: Text(dado.nome))),
-                          Expanded(
-                            child: Container(
-                                alignment: Alignment.center,
-                                constraints: const BoxConstraints.expand(),
-                                color: ColorLib.green.color,
-                                child: Text(dado.siglaPartido)),
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-              ],
+                      ]),
+                ]),
+              ),
             );
           },
         ),

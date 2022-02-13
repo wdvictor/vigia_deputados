181b41027400ba4430c9f5b22f9c302e2a54f751
victor.hugo <victorhpantunes@gmail.com>
2022-02-13 18:47:23 -0300

    .

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,5 @@
 // cSpell: ignore Camara camara cupertino
 
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:vigia_deputados/color_lib.dart';
@@ -58,122 +57,119 @@ class _HomePageState extends State<HomePage> {
             double axisSpacing = _size.height * 0.01;
             List<DeputadoDado> dados = snapshot.data!.dados;
             return SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 60),
-                child: CustomScrollView(
-                  slivers: [
-                    SliverList(
-                      delegate: SliverChildBuilderDelegate(
-                          (BuildContext context, int index) {
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 20, horizontal: 10),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Expanded(
-                                  child: CupertinoButton(
-                                onPressed: () {
-                                  showCupertinoModalPopup(
-                                      context: context,
-                                      builder: (context) =>
-                                          const MenuActionSheet());
-                                },
-                                child: const Icon(
-                                    CupertinoIcons.line_horizontal_3_decrease),
-                              )),
-                              const Expanded(
-                                flex: 5,
-                                child: CupertinoSearchTextField(
-                                  placeholder: 'Buscar',
-                                ),
+              child: CustomScrollView(
+                slivers: [
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                        (BuildContext context, int index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 20, horizontal: 10),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Expanded(
+                                child: CupertinoButton(
+                              onPressed: () {
+                                showCupertinoModalPopup(
+                                    context: context,
+                                    builder: (context) =>
+                                        const MenuActionSheet());
+                              },
+                              child: const Icon(
+                                  CupertinoIcons.line_horizontal_3_decrease),
+                            )),
+                            const Expanded(
+                              flex: 5,
+                              child: CupertinoSearchTextField(
+                                placeholder: 'Buscar',
                               ),
-                            ],
-                          ),
-                        );
-                      }, childCount: 1),
-                    ),
-                    SliverGrid.count(
-                      mainAxisSpacing: axisSpacing,
-                      crossAxisSpacing: axisSpacing,
-                      crossAxisCount: 2,
-                      children: [
-                        for (final DeputadoDado dado in dados)
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                CupertinoPageRoute(
-                                  builder: (BuildContext context) =>
-                                      DeputadoProfilePage(deputado: dado),
+                            ),
+                          ],
+                        ),
+                      );
+                    }, childCount: 1),
+                  ),
+                  SliverGrid.count(
+                    mainAxisSpacing: axisSpacing,
+                    crossAxisSpacing: axisSpacing,
+                    crossAxisCount: 2,
+                    children: [
+                      for (final DeputadoDado dado in dados)
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              CupertinoPageRoute(
+                                builder: (BuildContext context) =>
+                                    DeputadoProfilePage(deputado: dado),
+                              ),
+                            );
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Material(
+                              elevation: 10,
+                              color: CupertinoColors
+                                  .tertiarySystemGroupedBackground,
+                              borderRadius: BorderRadius.circular(20),
+                              child: Container(
+                                clipBehavior: Clip.antiAlias,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
                                 ),
-                              );
-                            },
-                            child: Padding(
-                              padding: const EdgeInsets.all(10),
-                              child: Material(
-                                elevation: 10,
-                                color: CupertinoColors
-                                    .tertiarySystemGroupedBackground,
-                                borderRadius: BorderRadius.circular(20),
-                                child: Container(
-                                  clipBehavior: Clip.antiAlias,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(20),
-                                  ),
-                                  child: Column(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceAround,
-                                    children: [
-                                      Expanded(
-                                        flex: 4,
-                                        child: Material(
-                                          shape: const CircleBorder(),
-                                          elevation: 11,
-                                          child: Container(
-                                            clipBehavior: Clip.antiAlias,
-                                            decoration: BoxDecoration(
-                                              borderRadius:
-                                                  BorderRadius.circular(15),
-                                            ),
-                                            height: _size.height * 0.13,
-                                            child: Hero(
-                                              tag: 'DEP-${dado.id}',
-                                              child: CircleAvatar(
-                                                backgroundImage:
-                                                    NetworkImage(dado.urlFoto),
-                                                radius: 45,
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                      ),
-                                      Expanded(
-                                        child: Center(
-                                          child: Text(
-                                            dado.nome,
-                                          ),
-                                        ),
-                                      ),
-                                      Expanded(
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Expanded(
+                                      flex: 4,
+                                      child: Material(
+                                        shape: const CircleBorder(),
+                                        elevation: 11,
                                         child: Container(
-                                          alignment: Alignment.center,
-                                          constraints:
-                                              const BoxConstraints.expand(),
-                                          color: ColorLib.green.color,
-                                          child: Text(dado.siglaPartido),
+                                          clipBehavior: Clip.antiAlias,
+                                          decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(15),
+                                          ),
+                                          height: _size.height * 0.13,
+                                          child: Hero(
+                                            tag: 'DEP-${dado.id}',
+                                            child: CircleAvatar(
+                                              backgroundImage:
+                                                  NetworkImage(dado.urlFoto),
+                                              radius: 45,
+                                            ),
+                                          ),
                                         ),
                                       ),
-                                    ],
-                                  ),
+                                    ),
+                                    Expanded(
+                                      child: Center(
+                                        child: Text(
+                                          dado.nome,
+                                        ),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Container(
+                                        alignment: Alignment.center,
+                                        constraints:
+                                            const BoxConstraints.expand(),
+                                        color: ColorLib.green.color,
+                                        child: Text(dado.siglaPartido),
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
                             ),
                           ),
-                      ],
-                    ),
-                  ],
-                ),
+                        ),
+                    ],
+                  ),
+                ],
               ),
             );
           },

7ba947218b4ae8ce07d6ac5b58b003c0ce1c8ea0
victor.hugo <victorhpantunes@gmail.com>
2023-06-21 11:25:36 -0300

    adding animation

--- lib/pages/homepage/home_page.dart
+++ lib/pages/homepage/home_page.dart
@@ -1,11 +1,6 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:vigia_deputados/helpers/color_lib.dart';
-import 'package:vigia_deputados/pages/homepage/homepage_widgets/deputados_widget.dart';
-import 'package:vigia_deputados/pages/homepage/homepage_widgets/partidos_widget.dart';
-import 'package:vigia_deputados/pages/homepage/homepage_widgets/proposicoes_widget.dart';
 import 'package:vigia_deputados/widgets/animated_button.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,96 +10,108 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  final double _initialPosition =
+      -500.0; // Posição inicial do menu (fora da tela)
+  final double _targetPosition =
+      200.0; // Posição final do menu (centro da tela)
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 400),
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
     return Scaffold(
-        body: Stack(
-      children: [
-        Container(
-          width: size.width,
-          height: size.height,
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage('images/CAMARA_01.jpg'))),
-        ),
-        BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-          child: Container(
-            color: Colors.white.withOpacity(0.1),
-            width: size.width,
-            height: size.height,
-            child: Column(children: [
-              const Spacer(),
-              SizedBox(
-                height: size.height * 0.3,
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    AnimatedButton(
-                      child: PartidosWidget(
-                        animationMilliseconds: 250,
-                      ),
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        AnimatedButton(
-                            animationMilliseconds: 500,
-                            child: DeputadosWidget()),
-                        AnimatedButton(
-                          animationMilliseconds: 750,
-                          child: ProposicoesWidget(),
-                        )
-                      ],
-                    ),
-                  ],
+      body: Stack(
+        children: [
+          AnimatedBuilder(
+            animation: _animationController,
+            builder: (context, child) {
+              final position = _animationController.value *
+                      (_targetPosition - _initialPosition) +
+                  _initialPosition;
+
+              return Positioned(
+                left: 0,
+                right: 0,
+                bottom: position,
+                child: child!,
+              );
+            },
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                AnimatedButton(
+                  animationMilliseconds: 500,
+                  child: MenuOptionButton(
+                    title: 'Partidos',
+                    callback: () {},
+                  ),
                 ),
-              ),
-              Expanded(
-                flex: 2,
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    AnimatedButton(
-                        animationMilliseconds: 1000,
-                        child: MenuOptionButton(
-                            title: 'Votações', callback: () {})),
-                    AnimatedButton(
-                        animationMilliseconds: 1250,
-                        child: MenuOptionButton(
-                            title: 'Lideres', callback: () {})),
-                  ],
+                AnimatedButton(
+                  animationMilliseconds: 750,
+                  child: MenuOptionButton(
+                    title: 'Partidos',
+                    callback: () {},
+                  ),
                 ),
-              )
-            ]),
+                AnimatedButton(
+                  animationMilliseconds: 1000,
+                  child: MenuOptionButton(
+                    title: 'Partidos',
+                    callback: () {},
+                  ),
+                ),
+                AnimatedButton(
+                  animationMilliseconds: 1250,
+                  child: MenuOptionButton(
+                    title: 'Partidos',
+                    callback: () {},
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
-    ));
+        ],
+      ),
+    );
   }
 }
 
 class MenuOptionButton extends StatelessWidget {
-  const MenuOptionButton(
-      {Key? key, required this.title, required this.callback})
-      : super(key: key);
+  const MenuOptionButton({
+    Key? key,
+    required this.title,
+    required this.callback,
+  }) : super(key: key);
+
   final String title;
   final VoidCallback callback;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       height: 70,
       decoration: BoxDecoration(
-          color: ColorLib.darkBlue.color,
-          //border: Border.all(color: ColorLib.darkBlue.color, width: 2),
-          borderRadius: BorderRadius.circular(10)),
+        color: ColorLib.darkBlue.color,
+        borderRadius: BorderRadius.circular(10),
+      ),
       child: Row(
         children: [
           const Spacer(
@@ -113,7 +120,10 @@ class MenuOptionButton extends StatelessWidget {
           Text(
             title,
             style: GoogleFonts.montserrat(
-                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 17),
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 17,
+            ),
           ),
           const Spacer(
             flex: 10,

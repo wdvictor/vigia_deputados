a45a399f8356022b7a794d415ae34cd98b83c6ce
victor.hugo <victorhpantunes@gmail.com>
2023-09-29 18:33:38 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,13 @@
 //cSpell:ignore cupertino RGBO
-import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:vigia_deputados/models/notifiers/deputados_notifier.dart';
 import 'package:vigia_deputados/pages/homepage/home_page.dart';
 
-bool shouldUseFirestoreEmulator = false;
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  if (shouldUseFirestoreEmulator) {
-    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
-  }
   runApp(const MyApp());
 }
 
